e635efa039e30b60c1594edbff0991939924f6da
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-07-26 20:57:05 +0200

    Time: 15 ms (59.77%), Space: 11.3 MB (10.78%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        
         vector<vector<int>> matrixReshape;
 	    matrixReshape.resize(r, vector<int>(c));
+        
         int row = 0, col = 0;
+        
         if(r*c!=mat.size()* mat[0].size()) return mat;
+        
         for(int i = 0 ;i < mat.size(); i++)
             for(int j = 0 ; j < mat[0].size(); j++)
             {
                 if(col == c) col = 0, row++;
                 matrixReshape[row][col++] = mat[i][j];
             }
-    return matrixReshape;
-                
+        
+      return matrixReshape;           
     }
 };
