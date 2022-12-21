79488dcd14496ca77e4e3368a9b6c511c5f2659c
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2022-12-21 16:46:30 +0200

    Time: 492 ms (33.45%), Space: 64.4 MB (87.47%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static const int N = 1e4 + 1;
+    vector<int> edge[N];
+    vector<int> color;
+    bool ans = 1;
+    void DFS(int node)
+    {
+        for(auto& child: edge[node])
+            if(!color[child]) color[child] = 2 - color[node], DFS(child);
+            else if(color[child] == color[node]){ ans = 0; return; }
+            //else DFS(child);
+    }
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        color.resize(n + 1);
+        
+        for(auto& r: dislikes) 
+            edge[r[0]].push_back(r[1]), edge[r[1]].push_back(r[0]);
+        
+        for(int i = 1; ans && i <= n ; i++)
+            if(!color[i]) DFS(i);
+        
+        
+        return ans;
+    }
+  
+};
