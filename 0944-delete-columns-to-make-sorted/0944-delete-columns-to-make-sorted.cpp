5b97831394d2abdeb64091ae267fdedf105f375a
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-01-03 17:09:36 +0200

    Time: 147 ms (13.87%), Space: 12.1 MB (72.46%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -3,17 +3,8 @@ public:
     int minDeletionSize(vector<string>& v) {
         int ans = 0;
         for(int i = 0 ; i < v.back().size() ; i++)
-        {
-            string s; 
-            
-            for(int j = 0 ; j < v.size() ; j++)  s.push_back(v[j].at(i));
-            
-            sort(s.begin(), s.end());
-            
-            for(int j = 0 ; j < v.size() ; j++) 
-                if(v[j].at(i) != s[j]) { ans++;  break; }
-            
-        }
+            for(int j = 1 ; j < v.size(); j ++)
+                if(v[j][i] < v[j - 1][i]){ ans++; break; }
         
         return ans;
     }
