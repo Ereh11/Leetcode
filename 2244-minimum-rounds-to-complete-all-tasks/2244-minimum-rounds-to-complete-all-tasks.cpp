116879fec19b7a25fe6ce43edad92fbcd7d85f9e
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-01-04 07:42:47 +0200

    Time: 509 ms (41.64%), Space: 103.7 MB (45.14%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -3,17 +3,16 @@ public:
     int minimumRounds(vector<int>& tasks) {
         int ans = 0;
         unordered_map<int, int> mp;
+        
         for(auto& it: tasks) mp[it]++;
         
-        for(auto& it: tasks){
-            while(1) 
-            {
-                if(mp[it] == 1) return -1;
-                else if(!mp[it]) break;
-                else if(mp[it] % 3 == 0 || mp[it] & 1) mp[it] -=3, ans++;
-                else mp[it] -=2, ans++;
-            }
+        for(auto& it: mp)
+        {
+            if(mp[it.first] == 1) return -1;
+            else if(!(mp[it.first] % 3)) ans += mp[it.first] / 3;
+            else ans += (mp[it.first] / 3) + 1;
         }
+        
         return ans;
     }
 };
