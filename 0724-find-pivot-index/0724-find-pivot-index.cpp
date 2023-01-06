e5073433a490af0c0686e2d940411544b173b58d
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-01-06 09:20:32 +0200

    Time: 52 ms (44.60%), Space: 31.2 MB (17.90%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -1,7 +1,21 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        int sumall = 0, sumleft = 0;
+        for(auto& it: nums) sumall +=it;
         
+        for(int i = 0 ; i <  nums.size() ; i++)
+        {
+            sumleft += nums[i];
+            if(sumleft - nums[i] == sumall - sumleft) return i;
+        }
+        
+        return -1;
+    }
+};
+
+/*
+        solution 1: using two vector prefix and suffix sum 
         vector<int> pfx(10001), sfx(10001); 
         pfx[0] = nums[0], sfx[nums.size() - 1] = nums.back();
         
@@ -14,6 +28,5 @@ public:
         for(int i = 0; i < nums.size(); i++)
             if(sfx[i] - nums[i] == pfx[i] - nums[i]) return i;
         
-        return -1;
-    }
-};
+        return -1
+ */
