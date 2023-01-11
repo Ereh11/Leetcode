1bf8b88b3d88d488664276f97d0d498fad6eaa8c
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-01-11 13:43:05 +0200

    Time: 0 ms (100.00%), Space: 5.9 MB (73.25%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-         
-        return (int)sqrt(num) == sqrt(num);
+        long long l = 0, r = num, sq = 0, mid = 0;
+        
+        while(l <= r)
+        {
+            mid = ((l + r) >> 1);
+            sq = mid * mid;
+            if(sq == num) return true;
+            if(sq > num) r = mid - 1;
+            else l = mid + 1;
+        }
+        return false;
     }
+    /*
+    return (int)sqrt(num) == sqrt(num);
+    
+    */
 };
