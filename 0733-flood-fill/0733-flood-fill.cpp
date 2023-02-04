f2450c1a7e1252168d6857dcacf468464c580926
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-05 01:25:23 +0200

    Time: 14 ms (40.96%), Space: 14.3 MB (28.11%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,36 @@
 class Solution {
 public:
-    bool valid(int r, int c, int n, int m){ return (r >= 0 && r < n && c >= 0 && c < m); }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    
+    int dx[4] = {1, -1, 0, 0};
+    int dy[4] = {0, 0, 1, -1};
+    int n, m;
+    bool valid(int r, int c){ return (r >= 0 && r < n && c >= 0 && c < m); }
+    
+    void BFS(int sr, int sc, int color, vector<vector<int>>& image)
     {
         queue<pair<int, int>> qu;
-        int dx[] = {1, -1, 0, 0};
-        int dy[] = {0, 0, 1, -1};
-        qu.push({sr, sc});
-        int n = image.size(), m = image[0].size();
-        vector<vector<bool>> vis(n, vector<bool> (m, 0));
-            
+        int colorsource = image[sr][sc];
+        qu.push({sr, sc});  image[sr][sc] = color;
+        
         while(!qu.empty())
         {
             pair<int, int> ft = qu.front(); qu.pop();
-            vis[ft.first][ft.second] = 1;
             for(int i = 0 ; i < 4 ; i++)
             {
                 int x = dx[i] + ft.first, y = dy[i] + ft.second;
-                if(valid(x, y, n, m) && !vis[x][y] && image[x][y] == image[sr][sc]) 
+                if(valid(x, y) && image[x][y] == colorsource) 
                     qu.push({x, y}), image[x][y] = color;
             }
         }
-        image[sr][sc] = color;
+        
+        
+    }
+    
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) 
+    {
+        if(color == image[sr][sc]) return image;
+        n = image.size(), m = image[0].size();
+        BFS(sr, sc, color, image);
         return image;
     }
 };
