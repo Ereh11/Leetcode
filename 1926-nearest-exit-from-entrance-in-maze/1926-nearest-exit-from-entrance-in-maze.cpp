a4e3cfddada9b0a47cbace8927f4f33ac2e6cb62
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-04 08:00:16 +0200

    Time: 138 ms (62.87%), Space: 30.5 MB (66.79%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -3,38 +3,36 @@ public:
     int dx[4] = {1, -1, 0, 0};
     int dy[4] = {0, 0, 1, -1};
     int n, m;
-    vector<vector<int>> dist;
     
     bool check(int r, int c) { return(r >= 0 && r < n && c >= 0 && c < m); }
     
-    int BFS(int r, int c, vector<vector<char>>& maze)
+    int BFS(int entrance_x, int entrance_y, vector<vector<char>>& maze)
     {
-        queue<pair<int, int>> qu; qu.push({r, c});
-        int ans = INT_MAX;
+        queue<pair<pair<int, int>, int>> qu; qu.push({{entrance_x, entrance_y}, 0});
+        
         while(!qu.empty())
         {
             auto ft = qu.front(); qu.pop();
-            maze[ft.first][ft.second] = '+';
+            int r = ft.first.first, c = ft.first.second, val = ft.second;
+            maze[r][c] = '+';
+            
             for(int i = 0 ; i < 4; i++)
             {
-                int x = ft.first + dx[i], y = ft.second + dy[i];
-                if((ft.first == n - 1 || ft.second == m - 1 || !ft.first || !ft.second) && (ft.first != r || ft.second != c))
-                    ans = min(ans, dist[ft.first][ft.second]);
+                int x = r + dx[i], y = c + dy[i];
+                if((r == n - 1 || c == m - 1 || !r || !c) && (r != entrance_x || c != entrance_y))
+                    return val;
                 
                 if(check(x, y) && maze[x][y] == '.')
-                {
-                    qu.push({x, y}), dist[x][y] = dist[ft.first][ft.second] + 1;
-                    maze[x][y] = '+';
-                } 
+                    qu.push({{x, y}, val + 1}),  maze[x][y] = '+';
             }
         }
         
-        return (ans == INT_MAX) ? -1 : ans;
+        return -1;
     }
     
     int nearestExit(vector<vector<char>>& maze, vector<int>& e) {
         n = maze.size(), m = maze[0].size();
-        dist.resize(n, vector<int>(m));
+        
         return BFS(e[0], e[1], maze);
     }
 };
