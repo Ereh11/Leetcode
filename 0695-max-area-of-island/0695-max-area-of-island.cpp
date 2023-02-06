f4a0a08d0fd36e7c396656b96f987cf7910c7040
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-06 07:30:52 +0200

    Time: 28 ms (40.53%), Space: 23.3 MB (55.44%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int n, m;
-    vector<vector<bool>> vis;
+    int n, m, ans = 0, mx = 0;
     int dx[4] = { 1, -1, 0, 0 };
     int dy[4] = { 0, 0, 1, -1 };
-    bool check(int r, int c){ return (r>=0 && r<n && c>=0 && c<m && !vis[r][c]);}
-    int BFS(int r, int c, vector<vector<int>>& grid)
+    bool check(int r, int c){ return (r >= 0 && r < n && c >= 0 && c < m);}
+    void DFS(int r, int c, vector<vector<int>>& grid)
     {
-        int ans= 0;
-        queue<pair<int, int>> qu; qu.push({r, c}), vis[r][c]= 1;
-        while(!qu.empty())
+        ans++, grid[r][c] = 2;
+        for(int i = 0 ; i < 4; i++) 
         {
-            pair<int, int> ft= qu.front(); 
-            qu.pop(), ans++;
-            for(int i=0 ; i < 4; i++) 
-            {
-                int x= dx[i]+ ft.first, y= dy[i]+ ft.second;
-                if(check(x, y) && grid[x][y]) qu.push({x, y}), vis[x][y]= 1;
-            }
+            int x = dx[i] + r, y = dy[i] + c;
+            if(check(x, y) && grid[x][y] == 1) DFS(x, y, grid);
         }
-        return ans;
+        mx = max(mx, ans);
     }
     int maxAreaOfIsland(vector<vector<int>>& grid) {
-        n= grid.size(), m= grid[0].size();
-        vis.resize(n, vector<bool>(m));
-        int mx= 0;
-        for(int i= 0 ; i < n ; i++)
-            for(int j= 0 ; j < m ; j++) 
-                if(!vis[i][j] && grid[i][j]) mx= max(mx, BFS(i, j, grid));
+        
+        n = grid.size(), m = grid[0].size();
+        for(int i = 0 ; i < n ; i++)
+            for(int j = 0 ; j < m ; j++) 
+                if(grid[i][j] == 1) ans = 0, DFS(i, j, grid);
+        
         return mx;
     }
 };
