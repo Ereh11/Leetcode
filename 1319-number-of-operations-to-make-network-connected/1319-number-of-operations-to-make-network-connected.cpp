b4ea0e9f4eeccfb0a66513ef6b05d77a35023cae
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-09 13:22:38 +0200

    Time: 472 ms (5.16%), Space: 37.3 MB (97.74%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -4,18 +4,17 @@ public:
     int find(int node) {
         return (node == parent[node] ? node: find(parent[node]));
     }
+    
     void Union(int node1, int node2) {
         int p1 = find(node1), p2 = find(node2);
         parent[p1] = p2;
     }
-    
     
     int makeConnected(int n, vector<vector<int>>& connections) 
     {
         parent.resize(n);
         int cnt = 0, ans = 0;
         for(int i = 0 ; i < n ; i++) parent[i] = i;
-        
         for(int i = 0 ; i < connections.size(); i++)
         {
             if(find(connections[i][0]) != find(connections[i][1])) 
@@ -23,7 +22,6 @@ public:
             else  cnt++;
         }
                 
-        
         for(int i = 1; i < n ; i++)
             if(find(i) != find(i - 1)) Union(i, i - 1), cnt--, ans++;
         
