25520a3bad47983d6489e9924e777b8057e43d0c
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-13 06:45:15 +0200

    Time: 203 ms (68.43%), Space: 65.1 MB (44.73%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> v[n + 1]; vector<bool> frq(n + 1);
+        vector<int> v[n + 1]; 
+        vector<bool> frq(n + 1);
         
-        for(auto& it: trust) v[it[1]].push_back(it[0]), frq[it[0]] = 1;
+        for(auto& it: trust) 
+            v[it[1]].push_back(it[0]), frq[it[0]] = 1;
         
         for(int i = 1; i <= n ; i++) 
             if(v[i].size() == n - 1 && !frq[i]) return i;
