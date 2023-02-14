d986c1e4f3a18e93918b1ccdc89943293caa9fee
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-02-14 16:00:10 +0200

    Time: 195 ms (56.58%), Space: 59.3 MB (28.37%) - LeetHub

---
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+   
+    set<int> ans;
+    int cnt = 0;
+    void DFS(int node, int sz)
+    {
+      
+        
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) 
+    {
+        int n = graph.size();
+        vector<int> edge[n], ans, safenode(n), outcome(n);
+        queue<int> qu;
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(auto& it: graph[i])  edge[it].push_back(i);
+            
+            outcome[i] = graph[i].size();
+            if(!outcome[i]) qu.push(i);
+        }
+        
+        while(!qu.empty())
+        {
+            int ft = qu.front();
+            qu.pop();
+            safenode[ft] = 1;
+            
+            for(auto& it: edge[ft])
+                if(--outcome[it] == 0) qu.push(it);
+        }
+        for(int i = 0 ; i < n ; i++)
+            if(safenode[i]) ans.push_back(i);
+        
+        return ans;
+    }
+};
+
+
+
+
+
+/*
+*/
