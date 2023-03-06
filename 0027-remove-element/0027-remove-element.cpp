15d105b074b8ba8fdfa171b1afdb8d5ea69b97cf
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-03-06 21:51:42 +0200

    Time: 7 ms (18.70%), Space: 8.7 MB (93.55%) - LeetHub

--- 0027-remove-element/0027-remove-element.cpp
+++ 0027-remove-element/0027-remove-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
+        
         int indx = 0;
         for(auto& it: nums)
             if(it != val) nums[indx++] = it;
