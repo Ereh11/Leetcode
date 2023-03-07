ce20d80d2218882b474aca8f1e5d1f6216967fce
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-03-07 06:31:49 +0200

    Time: 36 ms (38.67%), Space: 17.6 MB (42.83%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        int p1 = 0, p2 = 0, ans = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        
+        while(p1 < g.size() && p2 < s.size())
+        {
+            if(g[p1] <= s[p2])
+                ans++, p1++, p2++;
+            else if(g[p1] < s[p2]) 
+                p1++;
+            else 
+                p2++;
+        }
+        
+        return ans;
+    }
+};
