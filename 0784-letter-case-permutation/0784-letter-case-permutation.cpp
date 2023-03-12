b25700299974922a87189c0354507e83b9f9bda3
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-03-12 10:23:48 +0200

    Time: 14 ms (18.72%), Space: 12.2 MB (14.87%) - LeetHub

---
+++ 0784-letter-case-permutation/0784-letter-case-permutation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> res;
+    bool vis[13];
+    void solve(string &s, int indx, string ans)
+    {
+        if(indx == s.size()) {
+            res.push_back(ans);
+            return;
+        }
+        
+        if(s[indx] >= 48 && s[indx] <= 57) {
+            ans += s[indx];
+            solve(s, indx + 1, ans);
+        }
+        else {
+            
+            ans += toupper(s[indx]);
+            solve(s, indx + 1, ans);
+            ans.pop_back();
+
+            ans += tolower(s[indx]);
+            solve(s, indx + 1, ans);
+            ans.pop_back();
+        }
+    }
+    vector<string> letterCasePermutation(string s) {
+        
+        solve(s, 0, "");
+        return res;
+    }
+};
