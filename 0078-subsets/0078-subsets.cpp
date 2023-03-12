428e9d5745e69c5907e9815b238579960e0ff419
‪Hany Abdou Saad‬‏ <93304653+Ereh11@users.noreply.github.com>
2023-03-12 07:03:08 +0200

    Time: 3 ms (61.22%), Space: 7.6 MB (46.69%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    bool vis[11];
+    set<vector<int>> uset;
+    
+    void solve(vector<int> & nums, int indx, vector<int> &ans)
+    {
+        for(int i = indx ; i < nums.size() ; i++)
+        {
+            if(!vis[i])
+            {
+                vis[i] = 1;
+                ans.push_back(nums[i]);
+                if(uset.find(ans) == uset.end())
+                    uset.insert(ans);
+                
+                solve(nums, i + 1, ans);
+                ans.pop_back();
+                vis[i] = 0;
+            }
+        }
+    }
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> ans;
+        solve(nums, 0, ans);
+        
+        vector<vector<int>> res;
+        
+        for(auto& it: uset)
+            res.push_back(it);
+        
+        ans.empty();
+        res.push_back(ans);
+        return res;
+    }
+};
